aea386faf884d7cb0afddccc6bcc6bf2e917a477
jroquelaure <jonathanr@jfrog.com>
2016-12-13 13:48:01 +0100

    final after first webinar

--- prep-webinar.ps1
+++ prep-webinar.ps1
@@ -1,20 +1,21 @@
 cd .\pizza\
 
+#1 BUILD the generic flavor
 tar -cvf pizza-1.0.tgz exec
 cd ..
 cp .\pizza\pizza-1.0.tgz  .
 
-#upload the generic package to artifactory
+#2 UPLOAD the generic package to artifactory
 jfrog rt u pizza-1.0.tgz generic-local/jfrog/bintray/bintray-webinar/pizza/
 
-#build the docker image
+#3 BUILD the docker image
 docker build -t ubuntu:5001/pizza:1.0 ./pizza/
 
-#push the docker image
+#4 PUSH the docker image
 docker login ubuntu:5001 -u admin
 docker push ubuntu:5001/pizza:1.0
 
-#we suppose all tests have been done and prommote the image to prod repo
+#5 we suppose all tests have been done and PROMOTE the image to prod repo
 curl -uadmin -X POST "http://ubuntu:5001/artifactory/api/docker/docker-dev-local/v2/promote" -H "Content-Type: application/json" -T promote.json
 
 #OPTIONNAL : we retag this image as latest
